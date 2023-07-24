f2418762a061144f08dff808424562a35cc14963
gmagosto <137725643+gmagosto@users.noreply.github.com>
2023-07-24 11:48:55 -0500

    Update Agosto - Final Project.py

--- Agosto - Final Project.py
+++ Agosto - Final Project.py
@@ -7,7 +7,6 @@ def intro():
     global user_name
     user_name = input("\nHello! Welcome to the Guess the Number Game!")
     print("Are you ready to guess the number?")
-    time.sleep(2)
 
     
 # Function to ask if they want to play
@@ -15,6 +14,7 @@ def want_play():
     answer = input("\nEnter YAY or NAY: ")
     if answer == "YAY":
         print("\nHooray! Let's play!")
+        time.sleep(1)
     elif answer == "NAY":
         print("\nAwe, wrong answer. Let's play!")
     else:
@@ -24,10 +24,10 @@ def want_play():
        
 # Function for game
 def guess_number_game():
-    number = random.randint(1, 20)
+    number = random.randint(1, 10)
     guess = None
     while guess != number:
-        guess = input("Guess a number between 1 and 20: ")
+        guess = input("Guess a number between 1 and 10: ")
         guess = int(guess)
         if guess == number:
             print("\nCongratulations! You won!")
@@ -37,7 +37,7 @@ def guess_number_game():
             print("Guess must be an integer.")
             return 
               
-# Function to ask user to play again
+# Function to ask the user to play again
 def play_again():
     time.sleep(1)
     again = input("\nWould you like to play again? Enter YAY or NAY: ")
@@ -55,11 +55,13 @@ def play_again():
     if again not in ["YAY","NAY"]:
         print("Again, must be YAY or NAY.")
         return
+        play_again()
     
-# Section to begin game
+# Section to begin the game
 intro()
 want_play()
 guess_number_game()
+play_again()
 play_again()
 
 
